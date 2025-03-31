1f3c3cce23803dad62612d518eaf0ff7d931541b
Kirill Zhernov <ZhernovKR@yandex.ru>
2025-03-31 21:33:56 +0300

    Another fix for verilog-dump

--- tests_scr/scripts/convert.sh
+++ tests_scr/scripts/convert.sh
@@ -9,3 +9,5 @@ riscv64-unknown-elf-objcopy -O verilog --verilog-data-width 4 --strip-debug -S $
 awk 'NF { for(i=1; i<=NF; i++) print $i }' mem.mem > mem_formatted.mem
 
 grep "\S" mem_formatted.mem  >> $app.mem && rm mem_formatted.mem && rm mem.mem
+
+sed -i 's/^@F/@0/' $app.mem
